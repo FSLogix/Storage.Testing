2e4ccba1ab3f1b6d6824b6a47e781a6a0118eb66
Leee Jeffries <leee.jeffries@leeejeffries.com>
2018-08-01 21:11:04 +0100

    Amends to Powershell Automation Scripts - Test OU Structure Build

---
+++ Azure-Templates/Draft/active-directory-new-domain/DomainControllerConfig.ps1
@@ -0,0 +1,4 @@
+<# Custom Script for Windows to install a file from Azure Storage using the staging folder created by the deployment script #>
+New-Item -Path "C:\ScriptRun" -ItemType Directory
+"This is to prove that the powershell script ran" | Out-File -FilePath "C:\ScriptRun\IRan.txt"
+New-ADOrganizationalUnit -Name "FS" -Path "DC=fslogix,DC=local"
