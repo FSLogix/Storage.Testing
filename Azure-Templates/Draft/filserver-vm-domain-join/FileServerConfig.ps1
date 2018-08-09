e829ebe2cc2530dfc68699c535549d0816284841
Leee Jeffries <leee.jeffries@leeejeffries.com>
2018-08-09 16:36:45 +0100

    Amended fileserver powershell to configure data drive and fileshare

--- Azure-Templates/Draft/filserver-vm-domain-join/FileServerConfig.ps1
+++ Azure-Templates/Draft/filserver-vm-domain-join/FileServerConfig.ps1
@@ -12,9 +12,28 @@
 $logLoc = "C:\CustomPOSH_Logs"
 $null = New-Item -ItemType Directory -Path $logLoc -Force
 
-##Create folder share for FSLogix
-New-Item "D:\Shared" –type directory
-New-SMBShare –Name "Storage" –Path "D:\Shared" –ContinuouslyAvailable –FullAccess "fslogix.local\domainadmins" -ChangeAccess "fslogix.local\authentication users"
+##Data disk initialisation - NOTE, setup for the number of disks I know will be attached - just a single data disk in this scenario
+$newdisk = @(get-disk | Where-Object partitionstyle -eq 'raw')
+$Labels = @('Data')
 
+#Loop through any raw disks, create a filesystem and assign a driverletter
+for($i = 0; $i -lt $newdisk.Count ; $i++)
+{
 
+    $disknum = $newdisk[$i].Number
+    $dl = get-Disk $disknum | 
+       Initialize-Disk -PartitionStyle GPT -PassThru | 
+          New-Partition -AssignDriveLetter -UseMaximumSize
+    Format-Volume -driveletter $dl.Driveletter -FileSystem NTFS -NewFileSystemLabel $Labels[$i] -Confirm:$false
 
+}
+
+##Create folder share for FSLogix in the Data drive
+#Grab the driveletter from the created drive
+$driveLetter = $dl.DriveLetter + ":"
+New-Item "$driveLetter\Shared" –type directory
+If (Test-Path "$driverLetter\Shared") {
+    New-SMBShare –Name "Storage" –Path "D:\Shared" –ContinuouslyAvailable $true –FullAccess "fslogix.local\domain admins" -ChangeAccess "fslogix.local\authenticated users"
+} else {
+    "Failed to create File Share, this will need to be done manually" | Out-File -FilePath "$logLoc\Setup.log" -Append 
+}
