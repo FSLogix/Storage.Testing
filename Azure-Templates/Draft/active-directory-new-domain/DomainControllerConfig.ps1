8b6b4e2faa2ee97d35035a059517966f29c26259
Leee Jeffries <leee.jeffries@leeejeffries.com>
2018-08-09 15:35:30 +0100

    Added data disk to file server and Domain OU setup is complete

--- Azure-Templates/Draft/active-directory-new-domain/DomainControllerConfig.ps1
+++ Azure-Templates/Draft/active-directory-new-domain/DomainControllerConfig.ps1
@@ -1,4 +1,58 @@
-<# Custom Script for Windows to install a file from Azure Storage using the staging folder created by the deployment script #>
-New-Item -Path "C:\ScriptRun" -ItemType Directory
-"This is to prove that the powershell script ran" | Out-File -FilePath "C:\ScriptRun\IRan.txt"
-New-ADOrganizationalUnit -Name "FS" -Path "DC=fslogix,DC=local"
+######## 
+#Domain Controller Configuration
+#Copyright:         Free to use, please leave this header intact
+#Author:            Leee Jeffries
+#Company:           LJC (https://www.leeejeffries.com) 
+#Script help:       Designed to be run from Azure ARM Template but can be run standalone
+#Purpose:           Downloads and installed AD Connect silently and provisions an OU structure for the Domain Controller to support the testing environment
+#Usage:             Used for FSLogix storage testing
+########
+
+##Log File configuration
+$logLoc = "C:\CustomPOSH_Logs"
+$null = New-Item -ItemType Directory -Path $logLoc -Force
+
+##Download necessary files for Installation
+#Local directory to download files
+$downloadLoc = "$env:USERPROFILE\Downloads\Temp"
+$null = New-Item -ItemType Directory -Path $downloadLoc -Force
+ 
+##Location for files
+$downloadFiles = @()
+$downloadFiles += ("https://download.microsoft.com/download/B/0/0/B00291D0-5A83-4DE7-86F5-980BC00DE05A/AzureADConnect.msi")
+ 
+##Loop through the array and download all files
+foreach ($file in $downloadFiles) {
+    #Get filename of downloadable file
+    $fileName = $file.SubString($file.LastIndexOf("/")+1,($file.Length - $file.LastIndexOf("/"))-1)    
+    [Net.ServicePointManager]::SecurityProtocol = "tls12, tls11, tls"
+    Invoke-WebRequest -Uri $file -OutFile "$downloadLoc\$fileName"
+    
+    #Wait for Windows to complete renaming the file from temp
+    Start-Sleep -Second 5
+
+    if (Test-Path "$downloadLoc\$fileName") {
+        Start-Process -FilePath msiexec.exe -ArgumentList "/i","$downloadLoc\$fileName","/q" -Wait
+    } else {
+        "Could not find Azure AD Connect Installer - Perhaps it failed to download, Please install manually" | Out-File -FilePath "$logLoc\Setup.log" -Append 
+    }
+}
+
+#Run install for AD Connect
+Start-Process -FilePath msiexec -ArgumentList "/i","AzureADConnect.msi","/q" -Wait
+
+#Create OU structure
+#Citrix OU
+New-ADOrganizationalUnit -Name "Citrix" -Path "DC=fslogix,DC=local"
+New-ADOrganizationalUnit -Name "Infrastructure" -Path "OU=Citrix,DC=fslogix,DC=local"
+New-ADOrganizationalUnit -Name "MasterVDA" -Path "OU=Citrix,DC=fslogix,DC=local"
+New-ADOrganizationalUnit -Name "VDA" -Path "OU=Citrix,DC=fslogix,DC=local"
+Set-GPInheritance -Target "OU=VDA,OU=Citrix,DC=fslogix,DC=local" -IsBlocked Yes
+#LoginVSI
+New-ADOrganizationalUnit -Name "LoginVSI" -Path "DC=fslogix,DC=local"
+New-ADOrganizationalUnit -Name "Infrastructure" -Path "OU=LoginVSI,DC=fslogix,DC=local"
+New-ADOrganizationalUnit -Name "Launchers" -Path "OU=LoginVSI,DC=fslogix,DC=local"
+#General
+New-ADOrganizationalUnit -Name "Servers" -Path "DC=fslogix,DC=local"
+
+
--- Azure-Templates/Draft/filserver-vm-domain-join/FileServerConfig.ps1
+++ Azure-Templates/Draft/filserver-vm-domain-join/FileServerConfig.ps1
@@ -1,3 +1,20 @@
-<# Custom Script for Windows to install a file from Azure Storage using the staging folder created by the deployment script #>
-New-Item -Path "C:\ScriptRun" -ItemType Directory
-"This is to prove that the powershell script ran" | Out-File -FilePath "C:\ScriptRun\IRan.txt"
+######## 
+#File Server Configuration
+#Copyright:         Free to use, please leave this header intact
+#Author:            Leee Jeffries
+#Company:           LJC (https://www.leeejeffries.com) 
+#Script help:       Designed to be run from Azure ARM Template but can be run standalone
+#Purpose:           Configures the datadisk of the VM and configures the Share and permissions
+#Usage:             Used for FSLogix storage testing
+########
+
+##Log File configuration
+$logLoc = "C:\CustomPOSH_Logs"
+$null = New-Item -ItemType Directory -Path $logLoc -Force
+
+##Create folder share for FSLogix
+New-Item "D:\Shared" –type directory
+New-SMBShare –Name "Storage" –Path "D:\Shared" –ContinuouslyAvailable –FullAccess "fslogix.local\domainadmins" -ChangeAccess "fslogix.local\authentication users"
+
+
+
