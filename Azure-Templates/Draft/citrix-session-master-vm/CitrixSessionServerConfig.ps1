66a119e8b9350574dae2d791fcc670615cb44373
Leee Jeffries <leee.jeffries@leeejeffries.com>
2018-08-11 18:31:41 +0100

    Added Citrix Session Server and LoginVSI Launcher templates to solution

---
+++ Azure-Templates/Draft/citrix-session-master-vm/CitrixSessionServerConfig.ps1
@@ -0,0 +1,13 @@
+######## 
+#File Server Configuration
+#Copyright:         Free to use, please leave this header intact
+#Author:            Leee Jeffries
+#Company:           LJC (https://www.leeejeffries.com) 
+#Script help:       Designed to be run from Azure ARM Template but can be run standalone
+#Purpose:           Installs and Configures Citrix Virtual Apps and all roles automatically and silently
+#Usage:             Used for FSLogix storage testing
+########
+
+##Log File configuration
+$logLoc = "C:\CustomPOSH_Logs"
+$null = New-Item -ItemType Directory -Path $logLoc -Force
